e9f35e5c65333b531865368007b97b301d0543f5
mwong775 <mwong775@gmail.com>
2020-06-28 16:37:30 -0700

    new test file

---
+++ example.cc
@@ -0,0 +1,57 @@
+#include "cuckoofilter/src/cuckoofilter.h"
+#include "cuckoohashtable/hashtable/cuckoohashtable.hh"
+#include "cuckoopair.hh"
+#include <math.h>
+
+using namespace std;
+
+// generate n 64-bit random numbers for running insert & lookup
+void random_gen(int n, vector<uint64_t> &store, mt19937 &rd)
+{
+    store.resize(n);
+    for (int i = 0; i < n; i++)
+        store[i] = (uint64_t(rd()) << 32) + rd();
+}
+
+int main(int argc, char **argv)
+{
+
+    int seed = 1;
+    mt19937 rd(seed);
+
+    int size = 240;               // 240000 for ~91% load factor
+    int init_size = size / 0.95; // max load factor of 95%
+    cout << "init size: " << init_size << "\n";
+    cuckoo_pair<int, 12> cp(size);
+
+    vector<uint64_t> r;
+    vector<uint64_t> s;
+
+    // 64-bit random numbers to insert and lookup -> lookup_size = insert_size * 100
+    random_gen(size, r, rd);
+    random_gen(size * 100, s, rd);
+
+    // add set R to filter
+    for (auto c : r)
+    {
+    }
+
+        // lookup set S and count false positives
+    cout << "s lookup\n";
+    size_t total_queries = 0;
+    size_t false_queries = 0;
+    for (size_t i = 0; i < s.size(); i++)
+    {
+        // if ()
+        // {
+        //     false_queries++;
+        // }
+        total_queries++;
+    }
+    // Output the measured false positive rate
+    cout << "total fp's: " << false_queries << " out of " << total_queries << "\n";
+    cout << "false positive rate is " << 100.0 * false_queries / total_queries << "%\n";
+
+
+    return 0;
+}
