2c0ac8a08b2ef9c19974ee8b7fa73bfb5ca83340
mwong775 <mwong775@gmail.com>
2020-06-29 13:38:20 -0700

    add cmd line input for insertion size

--- example.cc
+++ example.cc
@@ -2,6 +2,7 @@
 #include "cuckoofilter/src/cuckoofilter.h"
 
 #include <math.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -15,17 +16,21 @@ void random_gen(int n, vector<uint64_t> &store, mt19937 &rd)
 
 int main(int argc, char **argv)
 {
+    if(argc <= 1) {
+        cout << "enter number of items to insert\n";
+        return 1;
+    }
 
     int seed = 1;
     mt19937 rd(seed);
 
     typedef uint64_t KeyType;
-    int size = 24;               // 240000 for ~91% load factor
+    int size = atoi(argv[1]);             // 240000 for ~91% load factor
     int init_size = size / 0.95; // max load factor of 95%
     cout << "init size: " << init_size << "\n";
-    cuckoo_pair<KeyType, 16> cp(size);
+    cuckoo_pair<KeyType, 12> cp(size);
     // original cuckoo filter for false positives comparison
-    cuckoofilter::CuckooFilter<size_t, 16> cf(init_size);
+    cuckoofilter::CuckooFilter<size_t, 12> cf(init_size);
 
     // cp.info();
 
@@ -37,22 +42,39 @@ int main(int argc, char **argv)
     random_gen(size * 100, s, rd);
 
     // add set R to filter
+    // cout << "R INSERT\n";
     for (auto c : r)
     {
         // cout << c << " ";
         cp.insert(c);
-        // cf.Add(c);
-        assert(cf.Add(c) == cuckoofilter::Ok);
+        cf.Add(c);
+        // assert(cf.Add(c) == cuckoofilter::Ok);
     }
 
+    // check for false negatives with set R
+    
+    cout << "R LOOKUP (FN)\n";
+    size_t false_negs = 0;
+    for (auto c : r)
+    {
+        if (!cp.lookup(c))
+        {
+            // cout << "fn tag: " << l << "\n";
+            false_negs++;
+        }
+    }
+    cout << "total fn's: " << false_negs << "\n";
+
     // lookup set S and count false positives
-    cout << "s lookup\n";
+
+    cout << "S LOOKUP (FP)\n";
     size_t total_queries = 0;
     size_t false_queries = 0;
     size_t false_comp = 0;
-    // for (size_t i = 0; i < s.size(); i++)
-    for (auto l : s)
+    for (size_t i = 0; i < s.size(); i++)
+    // for (auto l : s)
     {
+        auto l = s.at(i);
         if (cp.lookup(l))
         {
             // cout << "fp tag: " << l << "\n";
@@ -69,7 +91,11 @@ int main(int argc, char **argv)
          << ", fp rate: " << 100.0 * false_queries / total_queries << "%\n";
     cout << "Original CF: " << false_comp << " out of " << total_queries << ", fp rate: "
          << 100.0 * false_comp / total_queries << "%\n";
+    /*
+*/
 
+
+    cp.hashInfo();
     cp.info();
     cout << "orig. CF:\n"
          << cf.Info() << "\n";
